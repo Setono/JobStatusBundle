b23ce3ed65127b6901fd5fc49ba5596198983d2b
Joachim Løvgaard <joachim@loevgaard.dk>
2021-05-11 15:23:47 +0200

    Use interface instead of class

--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace Setono\JobStatusBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Setono\JobStatusBundle\Entity\Job;
+use Setono\JobStatusBundle\Entity\JobInterface;
 use Webmozart\Assert\Assert;
 
 class JobRepository extends ServiceEntityRepository implements JobRepositoryInterface
 {
     public function findRunningJobs(int $limit = 1000, int $offset = null): array
     {
-        $jobs = $this->findBy(['state' => Job::STATE_RUNNING], ['updatedAt' => 'DESC'], $limit, $offset);
-        Assert::allIsInstanceOf($jobs, Job::class);
+        $jobs = $this->findBy(['state' => JobInterface::STATE_RUNNING], ['updatedAt' => 'DESC'], $limit, $offset);
+        Assert::allIsInstanceOf($jobs, JobInterface::class);
 
         return $jobs;
     }
--- src/Repository/JobRepositoryInterface.php
+++ src/Repository/JobRepositoryInterface.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Setono\JobStatusBundle\Repository;
 
 use Doctrine\Persistence\ObjectRepository;
-use Setono\JobStatusBundle\Entity\Job;
+use Setono\JobStatusBundle\Entity\JobInterface;
 
 interface JobRepositoryInterface extends ObjectRepository
 {
     /**
-     * @psalm-return list<Job>
+     * @psalm-return list<JobInterface>
      */
     public function findRunningJobs(int $limit = 1000, int $offset = null): array;
 }
