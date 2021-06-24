c080278b5fcea7361bfa9781dbf7db1357f7bc37
Joachim Løvgaard <joachim@loevgaard.dk>
2021-06-24 09:13:03 +0200

    Add comment and phpdoc to fix Psalm

--- src/EventListener/Doctrine/ConvertMappedSuperclassEventListener.php
+++ src/EventListener/Doctrine/ConvertMappedSuperclassEventListener.php
@@ -7,10 +7,15 @@ namespace Setono\JobStatusBundle\EventListener\Doctrine;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Setono\JobStatusBundle\Entity\JobInterface;
 
+/**
+ * This class will make the Job entity an actual entity instead
+ * of a mapped superclass if it wasn't extended by the end user
+ */
 final class ConvertMappedSuperclassEventListener
 {
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
     {
+        /** @var class-string $class */
         $class = $eventArgs->getClassMetadata()->getName();
 
         if (is_a($class, JobInterface::class, true)) {
