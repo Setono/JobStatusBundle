ab2ef6c537fda0c8e0dea5bc94fe71a21bd93e48
Joachim Løvgaard <joachim@loevgaard.dk>
2021-07-14 11:14:49 +0200

    Added 'service exists' tests for all services

--- tests/SetonoJobStatusBundleTest.php
+++ tests/SetonoJobStatusBundleTest.php
@@ -9,12 +9,32 @@ use EventSauce\BackOff\BackOffStrategy;
 use EventSauce\BackOff\FibonacciBackOffStrategy;
 use Nyholm\BundleTest\BaseBundleTestCase;
 use Nyholm\BundleTest\CompilerPass\PublicServicePass;
+use Setono\JobStatusBundle\Command\ListCommand;
+use Setono\JobStatusBundle\Command\PruneCommand;
+use Setono\JobStatusBundle\Command\TimeoutCommand;
+use Setono\JobStatusBundle\EventListener\Doctrine\ConvertMappedSuperclassEventListener;
+use Setono\JobStatusBundle\EventListener\Doctrine\ValidateJobEventListener;
+use Setono\JobStatusBundle\EventSubscriber\CheckJobFinishedEventSubscriber;
+use Setono\JobStatusBundle\EventSubscriber\UpdateJobProgressEventSubscriber;
+use Setono\JobStatusBundle\EventSubscriber\Workflow\FinishJobEventSubscriber;
+use Setono\JobStatusBundle\EventSubscriber\Workflow\StartJobEventSubscriber;
+use Setono\JobStatusBundle\Factory\JobFactory;
+use Setono\JobStatusBundle\Factory\JobFactoryInterface;
+use Setono\JobStatusBundle\Manager\JobManager;
+use Setono\JobStatusBundle\Manager\JobManagerInterface;
+use Setono\JobStatusBundle\Repository\JobRepository;
+use Setono\JobStatusBundle\Repository\JobRepositoryInterface;
 use Setono\JobStatusBundle\SetonoJobStatusBundle;
+use Setono\JobStatusBundle\Twig\Extension;
+use Setono\JobStatusBundle\Twig\Runtime;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Twig\Extension\ExtensionInterface;
+use Twig\Extension\RuntimeExtensionInterface;
 
 /**
  * @covers \Setono\JobStatusBundle\SetonoJobStatusBundle
@@ -26,19 +46,87 @@ final class SetonoJobStatusBundleTest extends BaseBundleTestCase
         return SetonoJobStatusBundle::class;
     }
 
-    public function testBundleWithDifferentConfiguration(): void
+    /**
+     * @test
+     */
+    public function it_has_services(): void
     {
-        // Create a new Kernel
         $kernel = new TestKernel('dev', true);
         $kernel->boot();
 
         $container = $kernel->getContainer();
 
         $services = [
+            // back_off.xml
             'setono_job_status.back_off.fibonacci' => [
                 'interface' => BackOffStrategy::class,
                 'class' => FibonacciBackOffStrategy::class,
-            ]
+            ],
+
+            // command.xml
+            'setono_job_status.command.list' => [
+                'class' => ListCommand::class,
+            ],
+            'setono_job_status.command.prune' => [
+                'class' => PruneCommand::class,
+            ],
+            'setono_job_status.command.timeout' => [
+                'class' => TimeoutCommand::class,
+            ],
+
+            // event_listener.xml
+            'setono_job_status.event_listener.doctrine.validate_job' => [
+                'class' => ValidateJobEventListener::class,
+            ],
+            'setono_job_status.event_listener.doctrine.convert_mapped_superclass' => [
+                'class' => ConvertMappedSuperclassEventListener::class,
+            ],
+
+            // event_subscriber.xml
+            'setono_job_status.event_subscriber.check_job_finished_event_subscriber' => [
+                'class' => CheckJobFinishedEventSubscriber::class,
+                'interface' => EventSubscriberInterface::class,
+            ],
+            'setono_job_status.event_subscriber.update_job_progress_event_subscriber' => [
+                'class' => UpdateJobProgressEventSubscriber::class,
+                'interface' => EventSubscriberInterface::class,
+            ],
+            'setono_job_status.event_subscriber.workflow.finish_job_event_subscriber' => [
+                'class' => FinishJobEventSubscriber::class,
+                'interface' => EventSubscriberInterface::class,
+            ],
+            'setono_job_status.event_subscriber.workflow.start_job_event_subscriber' => [
+                'class' => StartJobEventSubscriber::class,
+                'interface' => EventSubscriberInterface::class,
+            ],
+
+            // factory.xml
+            'setono_job_status.factory.job' => [
+                'class' => JobFactory::class,
+                'interface' => JobFactoryInterface::class,
+            ],
+
+            // manager.xml
+            'setono_job_status.manager.job' => [
+                'class' => JobManager::class,
+                'interface' => JobManagerInterface::class,
+            ],
+
+            // repository.xml
+            'setono_job_status.repository.job' => [
+                'class' => JobRepository::class,
+                'interface' => JobRepositoryInterface::class,
+            ],
+
+            // twig.xml
+            'setono_job_status.twig.extension' => [
+                'class' => Extension::class,
+                'interface' => ExtensionInterface::class,
+            ],
+            'setono_job_status.twig.runtime' => [
+                'class' => Runtime::class,
+                'interface' => RuntimeExtensionInterface::class,
+            ],
         ];
 
         foreach ($services as $id => $values) {
@@ -48,7 +136,10 @@ final class SetonoJobStatusBundleTest extends BaseBundleTestCase
             self::assertNotNull($service);
 
             self::assertSame($values['class'], get_class($service));
-            self::assertInstanceOf($values['interface'], $service);
+
+            if (isset($values['interface'])) {
+                self::assertInstanceOf($values['interface'], $service);
+            }
         }
     }
 }
@@ -85,8 +176,21 @@ class TestKernel extends Kernel
                 ])
                 ->loadFromExtension('doctrine', [
                     'dbal' => [
-                        'url' => '%env(resolve:DATABASE_URL)%'
+                        'url' => 'sqlite:///%kernel.project_dir%/var/data.db'
                     ],
+                    'orm' => [
+                        'auto_generate_proxy_classes' => true,
+                        'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
+                        'auto_mapping' => true,
+                        'mappings' => [
+                            'SetonoJobStatusBundle' => [
+                                'is_bundle' => false,
+                                'type' => 'xml',
+                                'dir' => __DIR__ . '/../src/Resources/config/doctrine',
+                                'prefix' => 'Setono\JobStatusBundle\Entity'
+                            ]
+                        ]
+                    ]
                 ])
             ;
         });
